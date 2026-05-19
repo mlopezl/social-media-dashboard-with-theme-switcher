041272fc6bf7c19230680f9f7abe790acee3c037
maynor-lopez <maynor94l39@gmail.com>
2026-05-18 19:20:08 -0600

    Adding to focus state and keydown event to change mode using enter

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const toggle = document.getElementById('toggle');
 const body = document.body;
+const modeSwitch = document.querySelector('.mode__switch');
 
 
 toggle.addEventListener('change', () =>{
     if(toggle.checked){
-    body.classList.remove('dark');
-} else{
-    body.classList.add('dark');
-}
-})
+        body.classList.remove('dark');
+    } else{
+        body.classList.add('dark');
+    }
+});
 
+modeSwitch.addEventListener('keydown', (e) =>{
+    if(e.key === "Enter"){
+        toggle.checked = !toggle.checked;
+        toggle.dispatchEvent(
+            new Event('change', { bubbles: true })
+        )
+    }
+})
